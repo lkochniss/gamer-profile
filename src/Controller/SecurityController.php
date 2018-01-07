9b2e879bc3ab5cff483f7d6f5401bad661c70f7d
trulko2 <lukas.kochniss@trustedshops.de>
2018-01-07 11:04:00 +0100

    #30 add security layer

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+/**
+ * Class SecurityController
+ */
+class SecurityController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function login()
+    {
+        return $this->render(
+            'Security/login.html.twig'
+        );
+    }
+}
