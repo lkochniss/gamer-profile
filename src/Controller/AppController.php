18fc1ea8bff01f35de566cc5316fb62c88198e01
trulko2 <lukas.kochniss@trustedshops.de>
2018-01-08 19:02:43 +0100

    #45 split controller index action into two #40 make view a bit more responsive

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -16,12 +16,19 @@ class AppController extends Controller
      * @param GameRepository $gameRepository
      * @return Response
      */
-    public function index(GameRepository $gameRepository) {
-        return $this->render('homepage/index.html.twig', array(
-            'games' => [
-                'recentlyPlayed' => $gameRepository->getRecentlyPlayedGames(),
-                'mostPlayed'  =>  $gameRepository->getMostPlayedGames(9)
-            ]
+    public function recentlyPlayed(GameRepository $gameRepository) {
+        return $this->render('homepage/recentlyPlayed.html.twig', array(
+            'games' => $gameRepository->getRecentlyPlayedGames()
+        ));
+    }
+
+    /**
+     * @param GameRepository $gameRepository
+     * @return Response
+     */
+    public function mostPlayed(GameRepository $gameRepository) {
+        return $this->render('homepage/mostPlayed.html.twig', array(
+            'games' => $gameRepository->getMostPlayedGames(9)
         ));
     }
 }
