2cd77e23bf158d2372664a6ed5369bd413436f1d
Lukas Kochniss <lukas.kochniss@trustedshops.de>
2019-04-01 22:04:03 +0200

    fix getUser

--- src/Listener/AchievementListener.php
+++ src/Listener/AchievementListener.php
@@ -63,7 +63,7 @@ class AchievementListener
         $changeSet = $unitOfWork->getEntityChangeSet($entity);
 
         $achievementChangeSet = new AchievementChangeSet();
-        $achievementChangeSet->setUser($entity->getUser());
+        $achievementChangeSet->setUser($entity->getSteamUserId());
 
         foreach ($this->statProperties as $key => $statProperty) {
             if (array_key_exists($statProperty, $changeSet)) {
