d1752d995986b2e325ac4df31d415b3fd3374b54
trulko2 <lukas.kochniss@trustedshops.de>
2019-04-12 14:09:46 +0200

    add changeSet test for playtimeListener

--- tests/Listener/PlaytimeListenerTest.php
+++ tests/Listener/PlaytimeListenerTest.php
@@ -10,6 +10,7 @@ use App\Listener\PlaytimeListener;
 use App\Repository\OverallGameStatsRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\UnitOfWork;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -68,6 +69,68 @@ class PlaytimeListenerTest extends TestCase
 
         $argsMock->expects($this->never())
             ->method('getEntityManager');
+
+        $listener = new PlaytimeListener();
+        $listener->postUpdate($argsMock);
+    }
+
+    public function testPostUpdateShouldSaveTheChangeSet(): void
+    {
+        $steamUserId = 1;
+        $entity = new Playtime($steamUserId, new Game(1));
+
+        $argsMock = $this->createMock(LifecycleEventArgs::class);
+        $argsMock->expects($this->once())
+            ->method('getEntity')
+            ->willReturn($entity);
+
+        $overallGameStatsRepositoryMock = $this->createMock(OverallGameStatsRepository::class);
+
+        $entityManagerMock = $this->createMock(EntityManager::class);
+        $entityManagerMock->expects($this->any())
+            ->method('getRepository')
+            ->with(OverallGameStats::class)
+            ->willReturn($overallGameStatsRepositoryMock);
+
+        $unitOfWorkMock = $this->createMock(UnitOfWork::class);
+        $unitOfWorkMock->expects($this->once())
+            ->method('getEntityChangeSet')
+            ->with($entity)
+            ->willReturn([
+                'overallPlaytime' => [
+                    1,
+                    1
+                ],
+                'recentPlaytime' => [
+                    0,
+                    1
+                ]
+            ]);
+
+        $entityManagerMock->expects($this->once())
+            ->method('getUnitOfWork')
+            ->willReturn($unitOfWorkMock);
+
+        $argsMock->expects($this->any())
+            ->method('getEntityManager')
+            ->willReturn($entityManagerMock);
+
+        $expectedStats = new OverallGameStats($steamUserId);
+        $expectedStats->addToOverallPlaytime(1);
+        $expectedStats->addToRecentPlaytime(1);
+
+        $overallGameStats = new OverallGameStats(1);
+        $overallGameStats->addToOverallPlaytime(1);
+        $overallGameStatsRepositoryMock->expects($this->once())
+            ->method('findOneBy')
+            ->with([
+                'steamUserId' => $steamUserId
+            ])
+            ->willReturn($overallGameStats);
+
+        $overallGameStatsRepositoryMock->expects($this->once())
+            ->method('save')
+            ->with($expectedStats);
 
         $listener = new PlaytimeListener();
         $listener->postUpdate($argsMock);
