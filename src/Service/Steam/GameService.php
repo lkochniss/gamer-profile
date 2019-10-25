c589c938edfff2d082579344580c88abd73fe46e
lkochniss <lukas.kochniss@gmail.com>
2019-10-25 09:44:18 +0200

    Set a modifiedAt within update so it wont get stuck with a bunch of games

--- src/Service/Steam/GameService.php
+++ src/Service/Steam/GameService.php
@@ -61,6 +61,7 @@ class GameService
             $game->setName($gameInformation->getName());
             $game->setHeaderImagePath($gameInformation->getHeaderImagePath());
             $game->setCategories($gameInformation->getCategories());
+            $game->setModifiedAt();
         }
 
         $this->gameRepository->save($game);
@@ -77,7 +78,7 @@ class GameService
 
     public function updateOldest()
     {
-        $oldGames = $this->gameRepository->getLeastUpdatedGames(20);
+        $oldGames = $this->gameRepository->getLeastUpdatedGames(40);
 
         foreach ($oldGames as $oldGame) {
             $this->update($oldGame);
